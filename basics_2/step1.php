276637fb60dbe98d04ae8f755fdc6c11cb8e0cf9
rayan <rayanfayadhussein98@gmail.com>
2023-02-17 17:08:23 +0200

    step9

---
+++ basics_2/step1.php
@@ -0,0 +1,6 @@
+<?php
+//\n and PHP_EOL constant, which adds a newline character at the end of the string. This is done to ensure that each string is printed on a separate line. -->
+
+echo "Tomorrow I'll learn PHP global variables." ."\n";
+echo "This is a bad command: sudo rm -rf /"  . PHP_EOL; 
+?>
