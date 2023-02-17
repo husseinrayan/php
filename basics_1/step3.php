f28e31297e3720e2cfde2ac88c5a212e31f7f50a
rayan <rayanfayadhussein98@gmail.com>
2023-02-17 15:23:30 +0200

    step3

---
+++ basics_1/step3.php
@@ -0,0 +1,9 @@
+<?php
+$name="rayan";
+$email="rayanfayadhussein98@gmail.com";
+echo ' My name is  "'.$name.'" and my email is  "'.$email.'".';
+$height="100";
+$width="20";
+$area=$width * $height;
+echo $area
+?>
