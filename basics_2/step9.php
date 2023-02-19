1179eaef9d8b979a87edb1a20526f6d9f9090d8d
rayan <rayanfayadhussein98@gmail.com>
2023-02-19 20:19:09 +0200

    step9

---
+++ basics_2/step9.php
@@ -0,0 +1,13 @@
+<?php
+
+// Define the input number
+// $input_number = 222;
+$input_number = 16;
+
+// Compute the square root of the input number using the sqrt function
+$square_root = sqrt($input_number);
+
+// Print the square root of the input number
+echo "The square root of " . $input_number . " is " . $square_root . PHP_EOL;
+
+?>
