3a408ace0136b2f42a0b9f8394e88694b8a9ef58
rayan <rayanfayadhussein98@gmail.com>
2023-02-19 20:04:52 +0200

    step7

---
+++ basics_2/step7.php
@@ -0,0 +1,13 @@
+<?php
+
+$input_string = "I love Python programming. Python is awesome!";
+
+// Replace "Python" with "PHP"
+$output_string = str_replace("Python", "PHP", $input_string);
+
+// Replace "Python" with " PHP"
+
+
+echo $output_string . PHP_EOL;
+
+?>
